a3078a287d0d8569b55927994bf1c1f25d5a16f3
Allen <alleneben@gmail.com>
2019-12-10 15:19:07 -0500

    2nd commit

--- dit.js
+++ dit.js
@@ -2,8 +2,7 @@ var x=3;
 var z=10;
 let name= "DIT 2";
 
-let dit = [clearInterval
-    'Vincent','Freeman','Jesse','Evans',]
+let dit = ['Vincent','Freeman','Jesse','Evans',]
 var numbers = [1,2,3,4,5,6,7,8,9]
 
 let person1 = { 
@@ -17,3 +16,34 @@ let person2 = {
     color: 'yellow'
 }
 var persons = [person1, person2]
+
+
+for (let i = 0; i < 10; i++) {
+    
+    document.writeln(i)
+}
+var x=890
+var y = 90;
+
+if(x<y){
+    document.write('x is less than y')
+} else {
+    document.write('Oh x is not less than y')
+}
+
+let marks=70;
+
+
+if(marks >=80 && marks <=100){
+    document.write('A')
+} else if(marks >=60 &&marks<=80){
+    document.write('B')
+}else if(marks>90 && marks===90) {
+    document.write('F')
+} else {
+    document.write('hellooooo')
+}
+
+while (condition) {
+    
+}
